a790de6f1ecd8f29390796e39280bdd1e62f9414
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2026-05-19 18:34:33 -0400

    initial commit

---
+++ src/br/com/mramos/streams/Demo.java
@@ -0,0 +1,17 @@
+package br.com.mramos.streams;
+
+import java.util.Scanner;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        ListaNomes.listaDeNomes(s);
+
+        s.close();
+
+    }
+
+}
